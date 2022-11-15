3b8d3f5bf401aacc1fa4a4bd483509e470f1b121
ilaha2002 <ilaha_simmi_i@inbox.ru>
2022-11-15 15:02:07 +0400

    add js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const textInputField = document.querySelector
-('#text-input-field');
+const textInputField = document.querySelector('#text-input-field');
 const addButton = document.querySelector('#add-button');
 const todosContainer = document.querySelector('.todos-container');
+
+// addButton.forEach((element,i) => {
+//   element.addEventListener('click', () => {
+//     inputs[i].value=''
+//   });
+
+// });
+
+// inputs.forEach(element => {
+//   addEventListener('click',()=> {
+//     if(keycode = 'enter')
+//   });
+// });
 
 addButton.addEventListener('click', () => {
   if(textInputField.value.trim().length == '')
@@ -12,14 +24,16 @@ addButton.addEventListener('click', () => {
 
   todosContainer.appendChild(todoItemContainer);
 
-  // const todoText = document.createElement('p');
+
+
+  const todoText = document.createElement('p');
   todoText.id = 'text-input-field';
   todoText.innerText = textInputField.value;
   todoItemContainer.appendChild(todoText);
 
   todoText.addEventListener('dbclick', () => {
     todoText.classList.add('line-through');
-    // editButton.setAttribute("disabled", "disabled")
+   
   });
 
   const deleteButton = document.createElement('button');
@@ -38,67 +52,3 @@ addButton.addEventListener('click', () => {
   textInputField.value = "";
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const element = document.querySelector('.main-border');
-// const button = document.querySelector('.addbutton');
-// const firstInput = document.querySelector('input');
-
-// button.addEventListener('click', deleteValue);
-
-// firstInput.addEventListener('keyup', (event) => {
-//     if(event.target.keyCode = '13'){
-//       element.disabled = true;
-//     }
-// });
-
-
-
-
-// const button = document.querySelectorAll('button[data-target]');
-
-// for (const button) {
-//   // выбираем селектор, соответствующий текстовому полю
-//   const target = button.dataset.target;
-//   // если такой селектор определён...
-//   if (target) {
-//     // находим текстовое поле для этой кнопки
-//     const textarea = document.querySelector(target);
-    
-//     // если поле нашлось
-//     if (textarea) {
-//       // вешаем на кнопку обработчик события "клик"
-//       button.addEventListener('click', event => {
-//         // по клику меняем значение свойства "выключно" на противоположное
-//         textarea.disabled = !textarea.disabled;
-        
-//         textarea.focus();
-//       });
-//     }
-//   }
-// }
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// img=document.getElementById('addbuttonid')
-// img.onclick = function() {
-//   input_placetowrite = document.getElementById("placetowriteiid")
-//   placetowrite = input_placetowrite.value
-// }
